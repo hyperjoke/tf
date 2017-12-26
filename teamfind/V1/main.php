474a4efa20d1a946328ae039f53afdf9a974972f
HyperJoke Slovenia <34742817+hyperjoke@users.noreply.github.com>
2017-12-26 12:36:45 +0100

    uloading code

---
+++ teamfind/V1/main.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Avtor: DejanNOV
+Datum: 20:11:01 24. dec. 2017  2017
+Jezik: PHP
+REFNR: TF110AGC
+*/
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" type="text/css" href="css/mobile.css">
+    
+    <title>TF | main</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+    
+    <div style="position: fixed;" class="fixed menu menu-up-left">
+        <a href="#login"><span class="btn menu-login">LOGIN</span></a>
+        <a style="cursor:default;" href="#homepage"><span class="logo"></span></a>
+        <!--<img class="clearfix" src="res/logo.png"></img>-->
+    </div>
+    <div style="position: fixed;" class="fixed menu menu-r"></div>
+    
+    <div style="position: fixed;" class="fixed menu menu-dwn">
+        <a href="#join"><span class="ni-link bot-btn join">JOIN</span></a>
+        <a href="#profile"><span class="ni-link bot-btn profile">PROFILE</span></a>
+        <a href="#create"><span class="ni-link bot-btn create">CREATE</span></a>
+    </div>
+    
+    <div class=" buffer"></div>
+    
+    
+    
+    
+    Welcome to project TEAMFIND! - a brand new social network, to find your staff, friends, team members...,
+    <br>
+    Project ID: 1024
+    <br>
+    Referenčna številka datoteke: 
+    TF110AGD<br>
+    <img src="res/ref.png"></img>
+    
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    Reference number: TF110AGC
+    <br><br><br>
+    This project IS NOT publicaly avavible! <br>This content is makrked as NOT PUBLIC, if you see this content please report it to Dejan's Projets (expect on alfadejan.eu/r/****)
+</body>
+
+
+</html>
